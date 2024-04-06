e1ba37442b8b750edadd87894a035f327fba6ad0
DCRevResLabs <dc@revolutionresearch.com.au>
2024-04-07 06:41:37 +1000

    removed playground

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Navbar from "./components/Header/Navbar";
 import Home from "./components/Home";
 import Portfolio from "./components/Portfolio/Resume";
 import Contact from "./components/Contact/Contact";
-import Playground from "./components/Playground/Playground";
+// import Playground from "./components/Playground/Playground";
 import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
 
@@ -22,7 +22,7 @@ function App() {
           <Route path="/" exact element={<Home />} />
           <Route path="/portfolio" exact element={<Portfolio />} />
           <Route path="/contact" exact element={<Contact />} />
-          <Route path="/playground" exact element={<Playground />} />
+          {/* <Route path="/playground" exact element={<Playground />} /> */}
           <Route element={<NotFound />} />
         </Routes>
         <Footer />
--- client/src/components/Header/RightNav.js
+++ client/src/components/Header/RightNav.js
@@ -8,7 +8,7 @@ const linkItems = [
   { title: "Home", to: "/" },
   { title: "Portfolio", to: "/portfolio" },
   { title: "Contact", to: "/contact" },
-  { title: "Playground", to: "/playground" },
+  // { title: "Playground", to: "/playground" },
 ];
 
 const StyledHeader = styled.div`
